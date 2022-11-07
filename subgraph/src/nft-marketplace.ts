6f62a1d5a7d1f28b6bffca5ddcbbdf1891f59cf6
Mide001 <olamideisreal5@gmail.com>
2022-11-07 13:38:15 +0100

    updated

---
+++ subgraph/src/nft-marketplace.ts
@@ -0,0 +1,52 @@
+import { ListingCanceled, ListingCreated, ListingPurchased, ListingUpdated } from "../generated/NFTMarketplace/NFTMarketplace";
+import { store } from "@graphprotocol/graph-ts";
+import { ListingEntity } from "../generated/schema";
+
+export function handleListingCreated(event: ListingCreated): void {
+  const id = event.params.nftAddress.toHex() + "-" + event.params.tokenId.toString() + "-" + event.params.seller.toHex();
+
+  let listing = new ListingEntity(id);
+
+  listing.seller = event.params.seller;
+  listing.nftAddress = event.params.nftAddress;
+  listing.tokenId = event.params.tokenId;
+  listing.price = event.params.price;
+
+
+  listing.save();
+}
+
+export function handleListingCanceled(event: ListingCanceled): void{
+  const id = event.params.nftAddress.toHex() + "-" + event.params.tokenId.toString() + "-" + event.params.seller.toHex();
+
+  let listing = ListingEntity.load(id);
+  
+  if(listing) {
+    store.remove("ListingEntity", id);
+  }
+}
+
+export function handleListingUpdated(event: ListingUpdated): void{
+  const id = event.params.nftAddress.toHex() + "-" + event.params.tokenId.toString() + "-" + event.params.seller.toHex();
+
+  let listing = ListingEntity.load(id);
+
+  if(listing) {
+    listing.price = event.params.newPrice;
+
+    listing.save();
+  }
+}
+
+export function handleListingPurchased(event: ListingPurchased): void{
+  const id = event.params.nftAddress.toHex() +  "-" + event.params.tokenId.toString() + "-" + event.params.seller.toHex();
+  
+
+  let listing = ListingEntity.load(id);
+
+  if(listing){
+    listing.buyer = event.params.buyer;
+
+    listing.save();
+  }
+}
